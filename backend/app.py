b3d69d1231bc37045cc71f561343eccc6f72b71f
JunJianWoo <junwoo086@gmail.com>
2025-06-22 00:57:05 +1000

    Restructured extensions and initialised CORS

--- backend/app.py
+++ backend/app.py
@@ -1,40 +1,34 @@
 from flask import Flask
-from flask_migrate import Migrate
-from flask_restful import Api
 from dotenv import load_dotenv
-from extensions import db
-from models import Item
+from extensions import db, migrate, api, cors
 from resources.routes.item_price import ItemPriceTodayAPI
-from resources.routes.scrape_data import ScrapeDataAPI
 from resources.routes.items import SimilarItemAPI
-from flask_cors import CORS
 import os
+from scheduler.scrape_data import DataScrapingScheduler
 
-load_dotenv() 
 PORT_NUMBER = 8000
 
 def create_app(): 
+    
 # Flask Application Configuration
     app = Flask(__name__)
-    api = Api(app)
-    
-    CORS(app)
-
-    # Bind App and Database together
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+
     db.init_app(app)
-    migrate = Migrate(app,db)
+    migrate.init_app(app)
+    api.init_app(app)
+    cors.init_app(app,  resources={
+        "r/*": {"origins": os.environ['FRONTEND_URL']}
+    })
 
-    with app.app_context():
-        db.create_all()
-
-    api.add_resource(ItemPriceTodayAPI,"/api/dealstoday")
-    api.add_resource(ScrapeDataAPI, "/api/fetchdeals")
+    api.add_resource(ItemPriceTodayAPI, "/api/dealstoday")
     api.add_resource(SimilarItemAPI,"/api/similar/<string:desc>")
 
     return app
 
+
 if __name__ == "__main__":
+    load_dotenv() 
     app = create_app()
     app.run(port=PORT_NUMBER, debug=True)
 
--- backend/extensions.py
+++ backend/extensions.py
@@ -1,3 +1,9 @@
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+from flask_cors import CORS
 
 db = SQLAlchemy()
+migrate = Migrate()
+api = Api()
+cors = CORS()
